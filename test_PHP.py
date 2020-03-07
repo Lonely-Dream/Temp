a94a21e22ff6a1b0c584876a2252602628eb60bd
Cai-Zhenhui <20174698@stu.neu.edu.cn>
2020-03-08 01:49:46 +0800

    202003080131

--- test_PHP.py
+++ test_PHP.py
@@ -1,5 +1,7 @@
 import sys
 import json
+print(2)
 with open("result.txt","w") as f:
     for item in sys.argv:
         f.writelines(item)
+print(1)
--- test_PY.php
+++ test_PY.php
@@ -1,7 +1,7 @@
 <?php
 echo "今天是 " . date("Y/m/d") . "<br>";
 $params="/var/www/html/Temp/upload/1.png";
-$cmd="python3 predict.py ";
+$cmd="python3 test_PHP.py ";
 $output=exec($cmd.$params);
 echo $output;
 
