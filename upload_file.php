a00da980ebd7389379d76f75b8fc89306c2d8165
Cai-Zhenhui <52314437+Cai-Zhenhui@users.noreply.github.com>
2020-03-07 20:19:27 +0800

    Add files via upload

--- upload_file.php
+++ upload_file.php
@@ -6,5 +6,11 @@ else{
 	echo "Upload:".$_FILES["file"]["name"]."<br/>";
 	echo "Size:".$_FILES["file"]["size"]."<br/>";
 	echo "Stored in:".$_FILES["file"]["tmp_name"]."<br/>";
+	if(move_uploaded_file($_FILES["file"]["tmp_name"],$_FILES["file"][name])){
+		echo "True";
+	}
+	else {
+		echo "move faild";
+	}
 }
 ?>
