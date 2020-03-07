4a557ab635622548497b50fa7ca8b2a9bec68626
Cai-Zhenhui <20174698@stu.neu.edu.cn>
2020-03-07 23:52:41 +0800

    202003072352

--- test_PY.php
+++ test_PY.php
@@ -2,7 +2,8 @@
 echo "今天是 " . date("Y/m/d") . "<br>";
 $params="par1";
 $cmd="python3 test_PHP.py"
-$output=exec($cmd." ".$params);
+echo $cmd." ".$params;
+$output=exec("python3 test_PHP.py par1");
 dump($output);
 
 ?>
