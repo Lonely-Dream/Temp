ccc742c25e90f70951c36d5cc8e6ceb866704b53
Cai-Zhenhui <52314437+Cai-Zhenhui@users.noreply.github.com>
2020-03-07 21:13:40 +0800

    Add files via upload

--- upload_file.php
+++ upload_file.php
@@ -1,5 +1,4 @@
 <?php
-phpinfo();
 if($_FILES["file"]["error"]>0){
 	echo "Error:".$_FILES["file"]["error"]."<br/>";
 }
@@ -7,9 +6,20 @@ else{
 	echo "Upload:".$_FILES["file"]["name"]."<br/>";
 	echo "Size:".$_FILES["file"]["size"]."<br/>";
 	echo "Stored in:".$_FILES["file"]["tmp_name"]."<br/>";
-	echo "lisp";
-	echo $_FILES["file"]["tmp_name"];
-	echo "/var/www/html/Temp/upload/".$_FILES["file"]["name"];
+	echo $_FILES["file"]["tmp_name"]."<br/>";
+	echo "/var/www/html/Temp/upload/".$_FILES["file"]["name"]."<br/>";
+	if(file_exists($_FILES["file"]["tmp_name"])){
+		echo "exist"."<br/>";
+	}
+	else{
+		echo "not exit"."<br/>";
+	}
+	if(is_dir("/var/www/html/Temp/upload/"))
+		echo "dir"."<br/>";
+	}
+	else{
+		echo "not dir"."<br/>";
+	}
 	if(rename($_FILES["file"]["tmp_name"],"/var/www/html/Temp/upload/".$_FILES["file"]["name"])){
 		echo "True";
 	}
