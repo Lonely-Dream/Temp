168889b46d6cc5502cfafca879cd019170b87cd2
Cai-Zhenhui <20174698@stu.neu.edu.cn>
2020-03-08 01:47:19 +0800

    202003080131

--- test_PY.php
+++ test_PY.php
@@ -1,9 +1,8 @@
 <?php
 echo "今天是 " . date("Y/m/d") . "<br>";
-$params="upload/1.png";
-	$cmd="python3 predict.py ";
-	
-	$output=exec($cmd.$params);
-	echo $output;
+$params="/var/www/html/Temp/upload/1.png";
+$cmd="python3 predict.py ";
+$output=exec($cmd.$params);
+echo $output;
 
 ?>
