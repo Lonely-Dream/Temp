2cd50ea240836e9c9d0bedce99c550563a38d742
Cai-Zhenhui <52314437+Cai-Zhenhui@users.noreply.github.com>
2020-03-07 21:44:36 +0800

    Add files via upload

--- upload_file.php
+++ upload_file.php
@@ -8,13 +8,14 @@ else{
 	echo "Stored in:".$_FILES["file"]["tmp_name"]."<br/>";
 	echo $_FILES["file"]["tmp_name"]."<br/>";
 	echo "/var/www/html/Temp/upload/".$_FILES["file"]["name"]."<br/>";
+	
 	if(file_exists($_FILES["file"]["tmp_name"])){
 		echo "exist"."<br/>";
 	}
 	else{
 		echo "not exit"."<br/>";
 	}
-	if(is_dir("/var/www/html/Temp/upload/"))
+	if(is_dir("/var/www/html/Temp/upload/")){
 		echo "dir"."<br/>";
 	}
 	else{
