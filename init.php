73258f469f5e2f72928dfad96359c73e64924183
Cai-Zhenhui <20174698@stu.neu.edu.cn>
2020-03-09 01:18:14 +0800

    202003090118

--- init.php
+++ init.php
@@ -9,13 +9,23 @@ if($conn->connect_error){
 	die("connect faild:".$conn->connect_error);
 }
 
+//Create DataBase
+$sql = "CREATE DATABASE SmartPark default character set utf8";
+if($conn->query($sql)===TRUE){
+	echo "DataBase SmartPark created successfully!"."<br>";
+}
+else{
+	echo "DataBase SmartPark created faild!".$conn->error."<br>";
+}
+
+//Create users table
 $sql = "CREATE TABLE users(
 id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 tel VARCHAR(11) NOT NULL,
 password VARCHAR(16) NOT NULL,
 licenseplate VARCHAR(7) NOT NULL,
 facedata MEDIUMBLOB
-)";
+) DEFAULT CHARSET=utf8";
 if($conn->query($sql)===TRUE){
 	echo "TABLE users created successfully!"."<br>";
 }
@@ -23,11 +33,12 @@ else{
 	echo "TABLE created faild!".$conn->error."<br>";
 }
 
+//Create parkinfo table
 $sql = "CREATE TABLE parkinfo(
 licenseplate VARCHAR(7) NOT NULL,
 starttime DATETIME NOT NULL,
 endtime DATETIME
-)";
+) DEFAULT CHARSET=utf8";
 if($conn->query($sql)===TRUE){
 	echo "TABLE parkinfo created successfully!"."<br>";
 }
